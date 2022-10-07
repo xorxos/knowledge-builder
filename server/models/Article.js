12c7b3a0bd633181f4a6c0283e4a9afed30e9291
Parker <parkertleavitt@gmail.com>
2022-10-07 12:07:46 -0500

    create article and articleModule models

---
+++ server/models/Article.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const ArticleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please provide a title"],
+    maxlength: 200,
+  },
+  modules: [{ type: Schema.Types.ObjectId, ref: "Article Module" }],
+  status: {
+    type: String,
+    enum: ["pending, published, flagged"],
+    default: "pending",
+  },
+  createdBy: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: [true, "Please provide a user"],
+  },
+}, {timestamps: true});
+
+export default mongoose.model("Article", ArticleSchema);
---
+++ server/models/ArticleModule.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+export const ArticleModule = new mongoose.Schema({
+  moduleType: {
+    type: String,
+    enum: [
+      "header",
+      "subheader",
+      "paragraph",
+      "bullets list",
+      "numbered list",
+      "code block",
+      "image",
+      "text-image split",
+      "alert",
+    ],
+    default: "header",
+  },
+  position: {
+    type: Number,
+    req: [true, "Please provide a position for this module"],
+  },
+  mainText: {
+    type: String,
+    trim: true,
+  },
+  listText: {
+    type: [String],
+  },
+  alertColor: {
+    type: String,
+    enum: ["blue", "red", "yellow", "green", "orange"],
+  },
+  imagePath: {
+    type: String,
+  },
+  imageSplitDirection: {
+    type: String,
+    enum: ["left", "right"],
+    default: "right",
+  },
+  imageCaption: {
+    type: String,
+  },
+  articleId: {
+    type: mongoose.Types.ObjectId,
+    ref: "Article",
+    req: [true, "Please provide an article id"],
+  },
+  createdBy: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: [true, "Please provide a user"],
+  },
+}, {timestamps: true});
+
+export default mongoose.model("Article Module", ArticleModule);
