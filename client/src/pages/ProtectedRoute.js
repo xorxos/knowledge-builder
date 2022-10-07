0726c179ac660fdebb2b84be3bcc59770f241235
Parker <parkertleavitt@gmail.com>
2022-10-07 14:44:21 -0500

    add protected route component

---
+++ client/src/pages/ProtectedRoute.js
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom"
+
+const ProtectedRoute = ({children}) => {
+    //update when we start using context
+    const user = false;
+    if(!user) {
+        return <Navigate to='/landing'/>
+    }
+  return (
+  {children}
+  )
+}
+export default ProtectedRoute
