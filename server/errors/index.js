35e31952fac58f422e0456b4be2610d29d23840a
Parker <parkertleavitt@gmail.com>
2022-10-07 13:13:17 -0500

    create custom error classes

---
+++ server/errors/index.js
@@ -0,0 +1,6 @@
+import CustomAPIError from "./custom-api.js";
+import BadRequestError from "./bad-request.js";
+import UnauthenticatedError from "./unauthenticated.js";
+import NotFoundError from "./not-found.js";
+
+export { CustomAPIError, BadRequestError, UnauthenticatedError, NotFoundError };
