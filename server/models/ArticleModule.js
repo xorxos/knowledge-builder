fb2d737ad4e1a0ec68990afc614816bdfaeac762
Parker <parkertleavitt@gmail.com>
2022-10-07 13:04:31 -0500

    remove createdBy from ArticleModule model

--- server/models/ArticleModule.js
+++ server/models/ArticleModule.js
@@ -1,57 +1,55 @@
 import mongoose from "mongoose";
 
-const ArticleModuleSchema = new mongoose.Schema({
-  moduleType: {
-    type: String,
-    enum: [
-      "header",
-      "subheader",
-      "paragraph",
-      "bullets list",
-      "numbered list",
-      "code block",
-      "image",
-      "text-image split",
-      "alert",
-    ],
-    default: "header",
+const ArticleModuleSchema = new mongoose.Schema(
+  {
+    moduleType: {
+      type: String,
+      enum: [
+        "header",
+        "subheader",
+        "paragraph",
+        "bullets list",
+        "numbered list",
+        "code block",
+        "image",
+        "text-image split",
+        "alert",
+      ],
+      default: "header",
+    },
+    position: {
+      type: Number,
+      req: [true, "Please provide a position for this module"],
+    },
+    mainText: {
+      type: String,
+      trim: true,
+    },
+    listText: {
+      type: [String],
+    },
+    alertColor: {
+      type: String,
+      enum: ["blue", "red", "yellow", "green", "orange"],
+    },
+    imagePath: {
+      type: String,
+    },
+    imageSplitDirection: {
+      type: String,
+      enum: ["left", "right"],
+      default: "right",
+    },
+    imageCaption: {
+      type: String,
+    },
+    articleId: {
+      type: mongoose.Types.ObjectId,
+      ref: "Article",
+      req: [true, "Please provide an article id"],
+    },
   },
-  position: {
-    type: Number,
-    req: [true, "Please provide a position for this module"],
-  },
-  mainText: {
-    type: String,
-    trim: true,
-  },
-  listText: {
-    type: [String],
-  },
-  alertColor: {
-    type: String,
-    enum: ["blue", "red", "yellow", "green", "orange"],
-  },
-  imagePath: {
-    type: String,
-  },
-  imageSplitDirection: {
-    type: String,
-    enum: ["left", "right"],
-    default: "right",
-  },
-  imageCaption: {
-    type: String,
-  },
-  articleId: {
-    type: mongoose.Types.ObjectId,
-    ref: "Article",
-    req: [true, "Please provide an article id"],
-  },
-  createdBy: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    required: [true, "Please provide a user"],
-  },
-}, {timestamps: true});
+  { timestamps: true }
+);
 
 export default mongoose.model("Article Module", ArticleModuleSchema);
