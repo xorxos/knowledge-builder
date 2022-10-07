567e1375a408287cc0a79fd607768ccee5d17ad4
Parker <parkertleavitt@gmail.com>
2022-10-07 13:45:29 -0500

    add login logic to controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,8 +2,22 @@ import { StatusCodes } from "http-status-codes";
 import User from "../models/User.js";
 import { BadRequestError, UnauthenticatedError } from "../errors/index.js";
 
-const login = (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "login" });
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) throw new UnauthenticatedError("Invalid credentials");
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) throw new UnauthenticatedError("Invalid credentials");
+
+  const token = user.createJWT();
+  user.password = undefined;
+  res.status(StatusCodes.OK).json({ user, token });
 };
 
 const register = async (req, res) => {
