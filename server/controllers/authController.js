d9242a3a5ae9f3af7885e6d528515f54348c380d
Parker <parkertleavitt@gmail.com>
2022-10-07 13:34:53 -0500

    add register logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,11 +1,31 @@
 import { StatusCodes } from "http-status-codes";
+import User from "../models/User.js";
+import { BadRequestError, UnauthenticatedError } from "../errors/index.js";
 
 const login = (req, res) => {
   res.status(StatusCodes.OK).json({ msg: "login" });
 };
 
-const register = (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: "register" });
+const register = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  if ((!firstName, !lastName, !email, !password))
+    throw new BadRequestError("Please provide all values");
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) throw new BadRequestError("Email is already in use");
+
+  const user = await User.create({ firstName, lastName, email, password });
+  const token = user.createJWT();
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+    token,
+  });
 };
 
 const updateUser = (req, res) => {
