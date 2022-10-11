fa4fc7622b24b6dcba58f718028ac3fd707320cf
Parker <parkertleavitt@gmail.com>
2022-10-11 09:05:23 -0500

    create and add loading spinner

--- client/src/pages/dashboard/AllArticles.js
+++ client/src/pages/dashboard/AllArticles.js
@@ -1,14 +1,17 @@
 import { useEffect } from "react";
-import {StatsContainer, ArticlesContainer} from "../../components/";
+import { StatsContainer, ArticlesContainer } from "../../components/";
+import Loading from "../../components/Loading";
 import SearchContainer from "../../components/SearchContainer";
 import { useAppContext } from "../../context/appContext";
 
 const AllArticles = () => {
-  const {showStats, isLoading} = useAppContext();
+  const { showStats, isLoading } = useAppContext();
 
-  useEffect(()=>{
+  useEffect(() => {
     showStats();
-  }, [])
+  }, []);
+
+  if (isLoading) return <Loading center />;
 
   return (
     <>
